67d8b2bc657babd24d4f7e3b299a2fd6ef90e70c
Phong <phongphucdz@gmail.com>
2024-08-26 03:12:32 +0700

    commit code

---
+++ blogdetail.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caffee Chain</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/style6.css">
+</head>
+
+<body>
+    <header class="header">
+        <a href="#" class="logo">
+            <img src="/img/mani2-removebg-preview.png" alt="">
+        </a>
+        <nav class="navbar">
+            <a href="/index.php">Home</a>
+            <a href="/aboutus.php">About</a>
+            <a href="/view_products.php">Menu</a>
+            <a href="/review.php">review</a>
+            <a href="/blogs.php">Blog</a>
+            <a href="/contact.php">Contact</a>
+        </nav>
+        <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <a href="/shopping_cart.php" class="fas fa-shopping-cart" id="cart-btn"></a>
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+        <div class="search-form">
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label>
+        </div>
+     
+    </header>
+    <section class="home" id="home">
+        <div class="content">
+            
+        </div>
+    </section>
+    <section class="about" id="about">
+        <h1 class="heading"> <span>Blog</span> details</h1>
+        
+        <div class="row">
+        <h1 class="heading">Ba câu hỏi với Muna Mohammed của quán cà phê Eight50 của Canada</h1>
+            <div class="image">
+                <img src="https://dailycoffeenews.com/wp-content/uploads/2023/10/Muna.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Jen Roberts | October 12, 2023</h3>
+                <p> Born in Ethiopia as the granddaughter of a coffee farmer but raised mainly in Canada, Eight50 Coffee Founder Muna Mohammed had a profound experience when seeing coffee’s origins for the first time as an adult. 
+
+“It was something that always fascinated me, but I didn’t quite understand it,” Mohammed recently told Daily Coffee News. “I knew we drank coffee — I knew it was culturally integrated into our heritage — but it wasn’t until I visited Ethiopia after graduating college that I got to understand the complexities of coffee.”
+“I think COVID helped because it was a time where I was like, ‘It’s now or never — I’m going to launch this business,’” said Mohammed. “But I’m glad I took the time that I did [beforehand] because I started to really understand how the supply chain worked.”
+
+Mohammed drew upon her marketing background to develop a name for the business that may begin to tell a story and also lead to more questions. The Eight50 name references the year (850 CE) in which coffee was first discovered in the wild in Ethiopia, according to popular legend. 
+
+Said Mohammed, “I meet people all the time that think coffee comes from France or Italy.”  <br>
+                    Bright Acidity <br>
+                    Well balanced citrus flavour</p>
+                   
+                <a href="https://dailycoffeenews.com/2023/10/12/three-questions-with-muna-mohammed-of-canadas-eight50-coffee/" class="btn">Read More</a>
+            </div>
+
+        </div>
+        
+    </section>
+    <section class="footer">
+        <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         
+        </div>
+        <div class="links ">
+            <a href="/index.php ">home</a>
+            <a href="/aboutus.php ">about</a>
+            <a href="/view_products.php ">menu</a>
+            <a href="/product.php ">products</a>
+            <a href="/review.php ">review</a>
+            <a href="/contact.php ">contact</a>
+            <a href="/blogs.php ">blog</a>
+        </div>
+        <div class="credit">created by <span>Le Thanh Phong</span> | all right reserved</div>
+    </section>
+    <script src="js/script.js"></script>
+</body>
+</html>
